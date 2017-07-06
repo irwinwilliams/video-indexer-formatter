a2bd7896a3aa5479e65187734d917c05e01b7613
Irwin Williams <irwin.williams@teleios-systems.com>
2017-07-05 23:10:52 -0400

    working simple stripping happening, now

--- vi-outputter/src/app.js
+++ vi-outputter/src/app.js
@@ -1,5 +1,31 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+    constructor() {
+        this.message = "Hello, let's read some videoindexer output!";
+        this.vi_output_files = {};
+        this.vi_output = [];
+    }
+
+    parseFiles() {
+        console.log("INFO: parsing " + this.vi_output_files.length + " files");
+        for (const file of this.vi_output_files) {
+            let reader = new FileReader();
+            reader.App = this;
+            reader.onload = this.outputVIContent;
+            reader.readAsText(file);
+        }
+    }
+
+    outputVIContent(event) {
+        let reader = event.currentTarget;
+        let transcriptBlocks = JSON.parse(reader.result).breakdowns[0].insights.transcriptBlocks;
+        let result = '';
+        for (const block of transcriptBlocks) {
+            let blockResult = '';
+            for (const line of block.lines) {
+                blockResult += line.text + ' ';
+                result += line.text + ' ';
+            }
+            this.App.vi_output.push(blockResult);
+        }
+    }
 }
